5eaaa004f9899dbc5d4970bded31b44ffeac54cb
jrrodrigo421 <rjr8900@gmail.com>
2023-08-19 09:37:29 -0300

    parametros padrao de função

--- funcao/paramPadrao.js
+++ funcao/paramPadrao.js
@@ -5,10 +5,8 @@ function soma1(a, b, c) {
   b = b || 1
   c = c || 1
   return a + b + c
-
 }
 console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0));
-
 
 //Estrategia 2, 3 e 4 para gerar valor padrão
 function soma2(a, b, c) {
@@ -17,12 +15,11 @@ function soma2(a, b, c) {
   c = isNaN(c) ? 1 : c
   return a + b + c
 }
-
 console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0));
+
 
 // valor padrão do es2015
 function soma3(a = 1, b = 1, c = 1) {
   return a + b + c
 }
-
 console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0));
