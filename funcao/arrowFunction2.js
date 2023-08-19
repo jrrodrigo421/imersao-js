ee3c387a32c2e2a9342d220105b75a5c4f2c9fcd
jrrodrigo421 <rjr8900@gmail.com>
2023-08-19 13:33:46 -0300

    percorrendo array com map

--- funcao/arrowFunction2.js
+++ funcao/arrowFunction2.js
@@ -5,4 +5,16 @@ function Pessoa() {
     console.log(this.idade);
   }, 1000)
 }
-new Pessoa
+// new Pessoa
+
+// Exemplo do msn mozila: 
+const materials = ['Hydrogen', 'Helium', 'Lithium', 'Beryllium'];
+
+// console.log(materials.map((teste) => teste.length));
+// Expected output: Array [8, 6, 7, 9]
+console.log(materials.map((material) => material.replace('Hydrogen', 'Hidrogenio')))
+console.log(materials);
+
+const equipamentos = ['Furadeira', 'lanterna', 'capacete', 'bota', 'Facão']
+const mapeando = equipamentos.map((equipar) => equipar.charAt(1))
+console.log(mapeando);
