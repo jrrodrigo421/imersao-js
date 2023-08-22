5d1b3e2b85aa2c66efaeefdd0b641c6d5e3cd46e
jrrodrigo421 <rjr89000@gmail.com>
2023-08-22 18:24:52 -0300

    funcao construtura

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,27 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+  // atributo privado
+  let velocidadeAtual = 0
+
+  //metodo publico
+  this.acelerar = function () {
+    if (velocidadeAtual + delta <= velocidadeMaxima) {
+      velocidadeAtual += delta
+    } else {
+      velocidadeAtual = velocidadeMaxima
+    }
+    this.getVelocidadeAtual = function () {
+      return velocidadeAtual
+    }
+  }
+}
+const punto = new Carro
+punto.acelerar()
+console.log(punto.getVelocidadeAtual());
+punto.acelerar()
+punto.acelerar()
+punto.acelerar()
+punto.acelerar()
+punto.acelerar()
+punto.acelerar()
+punto.acelerar()
+console.log(punto.getVelocidadeAtual());
