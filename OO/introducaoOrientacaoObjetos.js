1b6fd0bbc86d852592628645126bb2d0e3ebc975
jrrodrigo421 <rjr89000@gmail.com>
2023-08-23 22:31:00 -0300

    Orientação a Objetos

---
+++ OO/introducaoOrientacaoObjetos.js
@@ -0,0 +1,21 @@
+// codigo nao executavel:
+
+//Procedural  
+processamento(v1, v2, v3)
+
+//OO
+objeto = {
+  v1,
+  v2,
+  v3,
+  processamento() {
+
+  }
+}
+objeto.processamento() // o foco passou a ser o objeto
+
+//Principios  importtantes:
+//1 Abstração;
+// 2 Encapsulamento;
+//3 Herança (prototype);
+//4 poliformismo;
