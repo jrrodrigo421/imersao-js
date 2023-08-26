93f64726a36a9db8eec2198293c148c597d9d5d6
jrrodrigo421 <rjr89000@gmail.com>
2023-08-26 15:39:37 -0300

    heranca exemplo3

---
+++ OO/heranca3.js
@@ -0,0 +1,25 @@
+const pai = { nome: 'Rodrigo', corDoCabelo: 'Preto' }
+
+const filho1 = Object.create(pai)
+filho1.nome = 'RodrigoJr'
+
+console.log(filho1.corDoCabelo);
+
+console.log(filho1);
+
+const filho2 = Object.create(pai, {
+  nome: { value: 'Ana', writable: false, enumerable: true }
+})
+console.log(filho2.nome);
+filho2.nome = 'Carla'
+console.log(`${filho2.nome} tem o cabelo ${filho2.corDoCabelo}`);
+console.log(Object.keys(filho1));
+console.log(Object.keys(filho2));
+console.log(Object.keys(pai));
+
+
+// pra saber se o atributo do objeto é dele mesmo ou veio por herança
+for (let key in filho2) {
+  filho2.hasOwnProperty(key) ?
+    console.log(key) : console.log(`Por herança de: ${key}`);
+}
