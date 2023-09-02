d164006e26d3a62b45577cbbfcf2df24f9bdbf57
jrrodrigo421 <rjr89000@gmail.com>
2023-09-02 18:13:01 -0300

    foreach

---
+++ array/foreach1.js
@@ -0,0 +1,14 @@
+const aprovados = ['Irene', 'Almerianda', 'Rodrigo', 'Adriana']
+aprovados.forEach(function (nome, indice) {
+  console.log(`${indice + 1} ${nome}`);
+})
+aprovados.forEach(function (nome, indice, array) {
+  console.log(`${indice + 1} ${nome}`);
+  console.log(array);
+})
+
+
+aprovados.forEach(nome => console.log(nome))
+const exibirAprovados = aprovado => console.log(aprovado);
+
+aprovados.forEach(exibirAprovados)
