a97398a110c324d6e4ddf9f470e8089cbbd9b2c2
jrrodrigo421 <rjr89000@gmail.com>
2023-09-03 15:33:08 -0300

    filter exemplo 1

---
+++ array/map4.js
@@ -0,0 +1,18 @@
+const { Long } = require("typeorm");
+
+const produtos = [
+  { nome: 'Notebook', preco: 2500.00, fragil: true },
+  { nome: 'PC', preco: 5000.00, fragil: true },
+  { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+  { nome: 'copo plastico', preco: 5.00, fragil: false },
+
+]
+
+// console.log(produtos.filter(function (p) {
+//   return p.nome.length < 3
+// }));
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro).filter(fragil));
